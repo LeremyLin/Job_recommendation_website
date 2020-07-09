cf7e6dd5dd1a0256cdef9212b9bfb079a0cdb35e
LeremyLin <linsanity@linsanitys-MacBook-Pro.local>
2020-07-09 23:47:55 +0800

    First commit

---
+++ jupiter/src/db/MySQLDBUtil.java
@@ -0,0 +1,17 @@
+package db;
+
+
+public class MySQLDBUtil {
+	private static final String INSTANCE = "recommend.cbpovae9v4er.us-east-2.rds.amazonaws.com";
+	private static final String PORT_NUM = "3306";
+	public static final String DB_NAME = "recommend_project";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "Linsanity4123";
+	public static final String URL = "jdbc:mysql://"
+			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+			+ "?user=" + USERNAME + "&password=" + PASSWORD
+			+ "&autoReconnect=true&serverTimezone=UTC";
+
+	
+
+}
---
+++ jupiter/target/jupiter/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Kobe!</h2>
+</body>
+</html>
